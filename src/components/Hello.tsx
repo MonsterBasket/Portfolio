450cabe0727de413fc63a2a04fdf483c518bbb59
MonsterBasket <blazza1@hotmail.com>
2023-02-22 22:26:43 +1100

    initialized project and created rough framework of required components

---
+++ src/components/Hello.tsx
@@ -0,0 +1,8 @@
+function Hello(){
+
+  return <div>
+    Hello
+  </div>
+}
+
+export default Hello
---
+++ src/components/Menu.tsx
@@ -0,0 +1,10 @@
+function Menu(){
+  return <div>
+    <div id="helloButton">Hello</div>
+    <div id="aboutButton">About</div>
+    <div id="projectsButton">Projects</div>
+    <div id="contactButton">Contact</div>
+  </div>
+}
+
+export default Menu;
