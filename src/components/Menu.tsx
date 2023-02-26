2228515fb7c31795123d77498f29afc315dfecac
MonsterBasket <blazza1@hotmail.com>
2023-02-26 22:16:13 +1100

    enhanced the roll effect to menu
    they're now 3D blocks that rotate as they roll backwards

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,6 +5,7 @@ function Menu(){
 
   const [menuTop, setMenuTop] = useState<number>(0)
   const [menuScale, setMenuScale] = useState<number>(1)
+  const [buttonRotate, setButtonRotate] = useState<number>(0)
 
   useScrollPosition(({ prevPos, currPos }) => {
     // get % of scroll to -500, then convert it to % of screen.innerHeight - button height
@@ -12,10 +13,12 @@ function Menu(){
     if (currPos.y === 0) {
       setMenuTop(0);
       setMenuScale(1);
+      setButtonRotate(0);
     }
     else if (currPos.y < -500) {
       setMenuTop(window.innerHeight - 30);
       setMenuScale(1);
+      setButtonRotate(1);
     }
     else {
       // a** + b** = c**,  ==>  b** = c** - a**  ==>  b = sqrt(c** - a**)
@@ -24,6 +27,7 @@ function Menu(){
       // c = radius of circle - we can just use 1 as it's a % conversion
       // Final equation: radius - scrollPos * windowHeight
       setMenuTop(1 - (currPos.y / -500) * (-window.innerHeight + 30))
+      setButtonRotate(currPos.y / -500)
       if (currPos.y / -500 < 0.5) {
         // b = scale of buttons
         // scale:         sqrt(radius -               scrollPos**)          + scaleAdjustment / (1 + scaleAdjustment)
@@ -32,25 +36,44 @@ function Menu(){
       else {
         setMenuScale((1 - Math.sqrt(1 - Math.pow(     currPos.y / -500,  2)) + 0.5) / 1.5)
       }
-      console.log(currPos.y / -500)
     }
   })
+
+  const helloStyle = {perspectiveOrigin: "200% center", perspective: `${menuScale * 100 + 300}px`};
+  const aboutStyle = {perspectiveOrigin: "100% center", perspective: `${menuScale * 100 + 300}px`};
+  const projectsStyle = {perspectiveOrigin: "0% center", perspective: `${menuScale * 100 + 300}px`};
+  const contactStyle = {perspectiveOrigin: "-100% center", perspective: `${menuScale * 100 + 300}px`};
+  const front = { transform: `rotateX(${buttonRotate * 180}deg) translateZ(15px)`, filter: `brightness(${2 * buttonRotate + 1})` };
+  const bottom = { transform: `rotateX(${buttonRotate * 180 - 90}deg) translateZ(15px)`, filter: `brightness(${2 * buttonRotate})` };
+  const back = { transform: `rotateX(${buttonRotate * 180 - 180}deg) translateZ(15px)`, filter: `brightness(${2 * buttonRotate - 1})` };
+
   
   return <div style={{ 
       top:`${menuTop}px`,
       transform: `scale(${menuScale})`
+      // perspective: `${menuScale * 300 + 200}px`
       }} id="menu">
-    <div className="menuButton helloButton">Hello</div>
-    <div className="menuButton aboutButton">About</div>
-    <div className="menuButton projectsButton">Projects</div>
-    <div className="menuButton contactButton">Contact</div>
+    <div style={helloStyle} className="menuButton helloButton">
+      <div style={front} className='menuButtonPanel'>Hello</div>
+      <div style={bottom} className='menuButtonPanel'>Hello</div>
+      <div style={back} className='menuButtonPanel'>Hello</div>
+    </div>
+    <div style={aboutStyle} className="menuButton aboutButton">
+      <div style={front} className='menuButtonPanel'>About</div>
+      <div style={bottom} className='menuButtonPanel'>About</div>
+      <div style={back} className='menuButtonPanel'>About</div>
+    </div>
+    <div style={projectsStyle} className="menuButton projectsButton">
+      <div style={front} className='menuButtonPanel'>Projects</div>
+      <div style={bottom} className='menuButtonPanel'>Projects</div>
+      <div style={back} className='menuButtonPanel'>Projects</div>
+    </div>
+    <div style={contactStyle} className="menuButton contactButton">
+      <div style={front} className='menuButtonPanel'>Contact</div>
+      <div style={bottom} className='menuButtonPanel'>Contact</div>
+      <div style={back} className='menuButtonPanel'>Contact</div>
+    </div>
   </div>
 }
 
 export default Menu;
-
-//a2 + b2 = c2
-//b2 = c2 - a2
-//c2 always = 1
-//a is the percentage of scroll
-//b is the amount of scale required
