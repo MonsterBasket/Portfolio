3a4020965b89fb207958f031cdbaa8a0fe1103f8
MonsterBasket <blazza1@hotmail.com>
2023-03-16 18:34:01 +1100

    started work on individual button left adjustment
    had to break them out of the grid container first, left is now being set via js rather than css

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -12,6 +12,20 @@ function Menu(){
   const [projectsTop, setProjectsTop] = useState<number>(0)
   const [contactTop, setContactTop] = useState<number>(0)
 
+  // these all need to change when screen width is changed, they currently only change on refresh
+  window.addEventListener('resize', adjustMenuLeft);
+  const [helloLeft, setHelloLeft] = useState<number>(window.innerWidth / 2 - 207)
+  const [aboutLeft, setAboutLeft] = useState<number>(window.innerWidth / 2 - 102.5)
+  const [projectsLeft, setProjectsLeft] = useState<number>(window.innerWidth / 2 + 2.5)
+  const [contactLeft, setContactLeft] = useState<number>(window.innerWidth / 2 + 107)
+
+  function adjustMenuLeft(){
+    setHelloLeft(window.innerWidth / 2 - (window.innerWidth > 500 ? 207 : 157))
+    setAboutLeft(window.innerWidth / 2 - (window.innerWidth > 500 ? 102.5 : 77.5))
+    setProjectsLeft(window.innerWidth / 2 + 2.5)
+    setContactLeft(window.innerWidth / 2 + (window.innerWidth > 500 ? 107 : 82))
+    }
+
   useScrollPosition(({ prevPos, currPos }) => {
     let windowLimit: number = window.innerHeight < 500 ? window.innerHeight : 500;
     if (currPos.y === 0) { // top of screen / not scrolled
@@ -75,10 +89,10 @@ function Menu(){
     }
   })
 
-  const helloStyle = { perspectiveOrigin: "200% center", perspective: `${menuScale * 100 + 300}px`, top: `${helloTop}px` };
-  const aboutStyle = { perspectiveOrigin: "100% center", perspective: `${menuScale * 100 + 300}px`, top: `${aboutTop}px` };
-  const projectsStyle = { perspectiveOrigin: "0% center", perspective: `${menuScale * 100 + 300}px`, top: `${projectsTop}px` };
-  const contactStyle = { perspectiveOrigin: "-100% center", perspective: `${menuScale * 100 + 300}px`, top: `${contactTop}px` };
+  const helloStyle = { perspectiveOrigin: "200% center", perspective: `${menuScale * 100 + 300}px`, top: `${helloTop}px`, left: `${helloLeft}px` };
+  const aboutStyle = { perspectiveOrigin: "100% center", perspective: `${menuScale * 100 + 300}px`, top: `${aboutTop}px`, left: `${aboutLeft}px` };
+  const projectsStyle = { perspectiveOrigin: "0% center", perspective: `${menuScale * 100 + 300}px`, top: `${projectsTop}px`, left: `${projectsLeft}px` };
+  const contactStyle = { perspectiveOrigin: "-100% center", perspective: `${menuScale * 100 + 300}px`, top: `${contactTop}px`, left: `${contactLeft}px` };
   const front = { transform: `rotateX(${buttonRotate * 180}deg) translateZ(15px)`, filter: `brightness(${2 * buttonRotate + 1})` };
   const bottom = { transform: `rotateX(${buttonRotate * 180 - 90}deg) translateZ(15px)`, filter: `brightness(${2 * buttonRotate})` };
   const back = { transform: `rotateX(${buttonRotate * 180 - 180}deg) translateZ(15px)`, filter: `brightness(${2 * buttonRotate - 1})` };
@@ -90,33 +104,25 @@ function Menu(){
       top:`${menuTop}px`,
       transform: `scale(${menuScale})`
       }} id="menu">
-    <a href="#hello">
-      <div style={helloStyle} className="menuButton helloButton">
-        <div style={front} className='menuButtonPanel'>Hello</div>
-        <div style={bottom} className='menuButtonPanel'>Hello</div>
-        <div style={back} className='menuButtonPanel'>Hello</div>
-      </div>
+    <a href="#hello" style={helloStyle} className="menuButton helloButton">
+      <div style={front} className='menuButtonPanel'>Hello</div>
+      <div style={bottom} className='menuButtonPanel'>Hello</div>
+      <div style={back} className='menuButtonPanel'>Hello</div>
     </a>
-    <a href="#about">
-      <div style={aboutStyle} className="menuButton aboutButton">
-        <div style={front} className='menuButtonPanel'>About</div>
-        <div style={bottom} className='menuButtonPanel'>About</div>
-        <div style={back} className='menuButtonPanel'>About</div>
-      </div>
+    <a href="#about" style={aboutStyle} className="menuButton aboutButton">
+      <div style={front} className='menuButtonPanel'>About</div>
+      <div style={bottom} className='menuButtonPanel'>About</div>
+      <div style={back} className='menuButtonPanel'>About</div>
     </a>
-    <a href="#projects">
-      <div style={projectsStyle} className="menuButton projectsButton">
-        <div style={front} className='menuButtonPanel'>Projects</div>
-        <div style={bottom} className='menuButtonPanel'>Projects</div>
-        <div style={back} className='menuButtonPanel'>Projects</div>
-      </div>
+    <a href="#projects" style={projectsStyle} className="menuButton projectsButton">
+      <div style={front} className='menuButtonPanel'>Projects</div>
+      <div style={bottom} className='menuButtonPanel'>Projects</div>
+      <div style={back} className='menuButtonPanel'>Projects</div>
     </a>
-    <a href="#contact">
-      <div style={contactStyle} className="menuButton contactButton">
-        <div style={front} className='menuButtonPanel'>Contact</div>
-        <div style={bottom} className='menuButtonPanel'>Contact</div>
-        <div style={back} className='menuButtonPanel'>Contact</div>
-      </div>
+    <a href="#contact" style={contactStyle} className="menuButton contactButton">
+      <div style={front} className='menuButtonPanel'>Contact</div>
+      <div style={bottom} className='menuButtonPanel'>Contact</div>
+      <div style={back} className='menuButtonPanel'>Contact</div>
     </a>
   </div>
 }
