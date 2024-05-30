b6d9c9861a214950b5551a01f2e0354ccf31942d
MonsterBasket <blazza1@hotmail.com>
2024-05-30 17:54:23 +1000

    made falling leaves to match the tree

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,66 +1,10 @@
-import { useEffect, useRef, useState } from "react"
+import React from "react";
 import Text from "./AboutText"
-import "./CSS/about.css"
 
-function About(){
-  const [delay, setDelay] = useState<number>(0)
-  const lastRender = useRef<number>(0)
-  const sway = useRef<number>(0.5);
-  const lastmousePos = useRef<number>(0);
-  const mousePos = useRef<number>(0);
-  const swayMod = useRef<number>(0)
-  const swayModDirection = useRef<number>(0)
-
-  useEffect(() => {
-    requestAnimationFrame((now) => animate(now))
-  }, [])
-  useEffect(() => {
-    window.addEventListener('mousemove', mouseCoords);
-    window.addEventListener('devicemotion', handleOrientation, true);
-    return () => {
-      window.removeEventListener('mousemove', mouseCoords);
-      window.removeEventListener('devicemotion', handleOrientation, true);
-    };
-  }, []);
-
-  function mouseCoords(e:MouseEvent){
-    mousePos.current = e.clientX
-    console.log(e.clientX)
-  }
-  function handleOrientation(e:any){
-    if(e.rotationRate.gamma) mousePos.current = Math.round(mousePos.current + e.rotationRate.gamma / 5)
-  }
-
-  function animate(now:number){
-    now *= 0.01;
-    const deltaTime = now - lastRender.current;
-    lastRender.current = now;
-    if (deltaTime) { //skips evaluations if no time has passed since last call (which strangely does happen)
-      let speed = (mousePos.current - lastmousePos.current) / deltaTime / 1000
-      sway.current *= 0.95
-      sway.current -= speed;
-        if (swayModDirection.current) swayMod.current += 0.001 * (sway.current + 1.5)
-        else swayMod.current -= 0.001 * (sway.current + 1.5)
-      if (swayMod.current >= 0.1) swayModDirection.current = 0;
-      if (swayMod.current <= 0) swayModDirection.current =1;
-      if (sway.current < -0.44) sway.current = -0.44
-      if (sway.current > 0.45) sway.current = 0.45
-      setDelay(sway.current - 0.5 + (swayMod.current - 0.05))
-      lastmousePos.current = mousePos.current;
-    }
-    setTimeout(() => requestAnimationFrame((now) => animate(now)), 33)
-  }
-
-  const treeStyle:React.CSSProperties = {
-    animationDelay: delay+"s",
-    right: sway.current
-  }
-  
+export default function About(){
   return <section id="About">
-    <div className="tree" style={treeStyle}></div>
-    <h3>TESTING: {mousePos.current}</h3>
-    <Text />
+    <div style={{height: '100%'}}>
+      <Text />
+    </div>
   </section>
 }
-
-export default About;
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,15 +1,91 @@
+import { ReactElement, useEffect, useRef, useState } from "react"
+import "./CSS/contact.css"
 import React from "react";
 
-function Contact(){
+export default function Contact(){
+  const [delay, setDelay] = useState<number>(0)
+  const lastRender = useRef<number>(0)
+  const sway = useRef<number>(0.5);
+  const lastmousePos = useRef<number>(0);
+  const mousePos = useRef<number>(0);
+  const swayMod = useRef<number>(0)
+  const swayModDirection = useRef<number>(0)
+  const leaves:ReactElement[] = []
+  const [renderLeaves, setLeaves] = useState<ReactElement[]>([])
+  const leafClasses:string[] = ["leaf", "blossom"]
+  const leafMoves:string[] = ["leafSway1", "leafSway2", "leafSway3"]
+  const leafSpins:string[] = ["leafSpin1", "leafSpin2", "leafSpin3"]
+
+  useEffect(() => makeLeaves, [])
+
+  useEffect(() => {
+    requestAnimationFrame((now) => animate(now))
+  }, [])
+  useEffect(() => {
+    window.addEventListener('mousemove', mouseCoords);
+    window.addEventListener('devicemotion', handleOrientation, true);
+    return () => {
+      window.removeEventListener('mousemove', mouseCoords);
+      window.removeEventListener('devicemotion', handleOrientation, true);
+    };
+  }, []);
+
+  function makeLeaves(){
+    console.log("hello leaves")
+    for (let i = 0; i < 60; i++) {
+      const depth = Math.random() + 0.5;
+      const fallSpeed = Math.random() * 5 + (window.innerHeight / 50) - 5 * (depth +1);
+      const startTop = Math.random() * -fallSpeed;
+      const leftPos = Math.random() * window.innerWidth + 'px';
+      const leafClass = leafClasses[Math.floor(Math.random() * 2)]
+      const spin = leafSpins[Math.floor(Math.random() * 3)]
+      const move = leafMoves[Math.floor(Math.random() * 3)]
+      const spinSpeed = Math.random() * 3 + 4 + 's'
+      const moveSpeed = Math.random() * 4 + 5 + 's'
+      leaves[i] = <div className="leafContainer" key={i} style={{animationDuration:fallSpeed+'s', animationDelay:startTop+'s', transform:`scale(${depth})`, zIndex:Math.floor((depth - 0.5) * 10), left:leftPos}}>
+        <div className={leafClass} style={{animationName:`${spin}, ${move}`, animationDuration:`${spinSpeed}, ${moveSpeed}`, filter:`blur(${depth * 3 - 2}px)`}}></div>
+      </div>
+    }
+    setLeaves([...leaves])
+  }
+
+  function mouseCoords(e:MouseEvent){
+    mousePos.current = e.clientX
+  }
+  function handleOrientation(e:any){
+    if(e.rotationRate.gamma) mousePos.current = Math.round(mousePos.current + e.rotationRate.gamma / 5)
+  }
+
+  function animate(now:number){
+    now *= 0.01;
+    const deltaTime = now - lastRender.current;
+    lastRender.current = now;
+    if (deltaTime) { //skips evaluations if no time has passed since last call (which strangely does happen)
+      let speed = (mousePos.current - lastmousePos.current) / deltaTime / 1000
+      let tempSway = sway.current * 0.95
+      let tempSwayMod = swayMod.current
+      tempSway -= speed;
+        if (swayModDirection.current) tempSwayMod += 0.001 * (tempSway + 1.5)
+        else tempSwayMod -= 0.001 * (tempSway + 1.5)
+      if (tempSwayMod >= 0.1) swayModDirection.current = 0;
+      if (tempSwayMod <= 0) swayModDirection.current =1;
+      if (tempSway < -0.43) tempSway = -0.43
+      if (tempSway > 0.43) tempSway = 0.43
+      setDelay(tempSway - 0.5 + (tempSwayMod - 0.05))
+      sway.current = tempSway
+      swayMod.current = tempSwayMod
+      lastmousePos.current = mousePos.current;
+    }
+    setTimeout(() => requestAnimationFrame((now) => animate(now)), 33)
+  }
+
+  
   return <section id="Contact">
-    <div style={{height: '100%'}}>
-      <article>
-        <h2>Contact</h2>
-        <a href="https://www.linkedin.com/in/james-blaskett/">LinkedIn</a><br/><br/>
-        <a href="mailto:jmblasket@gmail.com" target="_blank">Email</a>
-      </article>
-    </div>
+    <div className="tree" style={{animationDelay: delay+"s"}}></div>
+    {renderLeaves}
+    <h3>TESTING: {mousePos.current}</h3>
+    <h2>Contact</h2>
+    <a href="https://www.linkedin.com/in/james-blaskett/">LinkedIn</a><br/><br/>
+    <a href="mailto:jmblasket@gmail.com" target="_blank">Email</a>
   </section>
 }
-
-export default Contact;
